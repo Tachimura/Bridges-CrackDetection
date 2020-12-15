5ce0ab627e5c78b9348554f05262196f682f5bde
Tachimura <62809228+Tachimura@users.noreply.github.com>
2020-12-15 12:01:43 +0100

    Add files via upload

--- Environment/test_dependencies.py
+++ Environment/test_dependencies.py
@@ -2,11 +2,11 @@
 Quick script to test if any project dependencies are missed
 """
 if __name__ == '__main__':
-    try:
-        import numpy
-        import scipy
-        import matplotlib
-	import torch
-        print("All dependecies are satisfied!")
-    except ModuleNotFoundError as e:
-        print("Some dependencies missed:{}".format(e))
+	try:
+		import numpy
+		import scipy
+		import matplotlib
+		import torch
+		print("All dependecies are satisfied!")
+	except ModuleNotFoundError as e:
+		print("Some dependencies missed:{}".format(e))
